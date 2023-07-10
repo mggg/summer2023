3b29bfcc1cc057d1fbf7a43a260c3a45072c3011
roryhe <roryerlich@gmail.com>
2023-07-10 11:57:30 -0400

    set up ballot clustering folder

---
+++ ballot_clustering/ballots.py
@@ -0,0 +1,146 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import itertools
+from numpy.random import choice
+import random
+from collections import Counter
+import numpy as np
+
+'''
+This class generates balllots for PL and BT ballot types 
+'''
+
+class BallotGen:∏
+
+  def __init__(self,num_ballots, cand_list, voter_proportion_by_race, cand_support_interval):
+    self.num_ballots = num_ballots
+    self.cand_list = cand_list
+    self.voter_proportion_by_race = voter_proportion_by_race
+    self.cand_support_interval = cand_support_interval
+    self.list_of_ballot_distributions = []
+
+
+
+  def pl_ballots(self):
+      """ Returns a list of PL ballots of length num_ballots, 
+      with size of blocks proportional to voter_proportion_by_race """
+
+      ballots_list = []
+      for race in self.voter_proportion_by_race.keys():
+          num_ballots_race = int(self.num_ballots*self.voter_proportion_by_race[race])##this computes number of voters in this race/block
+          cand_support_vec = list(self.cand_support_interval[race].values())##creates the interval of probabilities for candidates supported by this block
+          for j in range(num_ballots_race):
+              x = tuple(choice(self.cand_list, len(self.cand_list), replace = False, p = cand_support_vec))
+              ballots_list.append(x)
+      return ballots_list
+  
+  def bt_ballots(self): 
+    """ Returns a list of BT ballots of length num_ballots, 
+      with size of blocks proportional to voter_proportion_by_race """
+
+    n=len(self.cand_list)
+    k=0
+    permutations = list(itertools.permutations(self.cand_list))
+    for combo in permutations:##computes (inverse of) the constant of proportionality
+      m=1
+      for i in range(n):
+        for j in range(i+1,n):
+          l=0
+          for race in self.voter_proportion_by_race.keys():
+            l = l+self.voter_proportion_by_race[race]*(self.cand_support_interval[race][combo[i]]/(self.cand_support_interval[race][combo[i]]+self.cand_support_interval[race][combo[j]]))
+          if j!=i:
+            m=m*l
+      k=k+m
+
+    weights = []
+
+    for combo in permutations:
+      prob=1
+      for i in range(n):
+        for j in range(i+1,n):
+          l=0
+          for race in self.voter_proportion_by_race.keys():
+            l = l+self.voter_proportion_by_race[race]*(self.cand_support_interval[race][combo[i]]/( self.cand_support_interval[race][combo[i]]+self.cand_support_interval[race][combo[j]]))
+          prob=prob*l
+      weights.append(prob/k)##we're giving each permutation of cand_list a weight
+
+    x = choice(range(len(permutations)), self.num_ballots, replace=True, p = weights)
+    return list(permutations[i] for i in x)
+
+    
+  
+  
+  def pl_ballot_count(self,num_elections, normalize = False):
+    """ Returns a list of dictionaries of length num_elections, where each dictionary gives the count of each ballot type
+     for that election. If normalize == true, divides all counts by number of ballots in election (so gives the 
+     distribution of ballot types)"""
+
+    # create list of lists of all possible ballots, then default dictionary where count of each ballot is 0
+    # note that we exclude ballots where num_cands - 1 candidates are ranked 
+    ballot_perms = [list(itertools.permutations(self.cand_list, num_ranked)) 
+                         for num_ranked in range(1, len(self.cand_list) + 1) if num_ranked != len(self.cand_list) - 1]
+
+    # consolidates ballot_perms into a single list 
+    cand_p_list = list(itertools.chain.from_iterable(ballot_perms))
+
+    cand_p_dict ={}
+    for i in cand_p_list:
+        cand_p_dict[i] = 0
+
+    elections_pl = []
+    for i in range(num_elections):
+        pl_ballots = self.pl_ballots()
+
+        # create a dictionary-like object that stores the count of each ballot type
+        pl_perf_count = Counter(pl_ballots)
+
+        # takes the union of this with the empty dictionary, giving priority to the counts in the latter 
+        pl_perf_count = cand_p_dict | pl_perf_count
+        
+        # Normalize 
+        if normalize == True:
+          for key in pl_perf_count.keys():
+            pl_perf_count[key] = pl_perf_count[key] / self.num_ballots
+        elections_pl.append(pl_perf_count)
+    
+    self.list_of_ballot_distributions = elections_pl
+    return elections_pl
+  
+  
+
+
+  def bt_ballot_count(self,num_elections,normalize = False):   
+    """ Returns a list of dictionaries of length num_elections, where each dictionary gives the count of each ballot type
+     for that election. If normalize == true, divides all values by number of ballots in election"""
+    # TODO: consolidate with pf_ballot_count (they basically do the same thing)
+
+
+    # create list of lists of all possible ballots, then default dictionary where count of each ballot is 0
+    
+    # note that we exclude ballots where num_cands - 1 candidates are ranked 
+    ballot_perms = [list(itertools.permutations(self.cand_list, num_ranked)) 
+                         for num_ranked in range(1, len(self.cand_list) + 1) if num_ranked != len(self.cand_list) - 1]
+
+    # consolidates ballot_perms into a single list 
+    cand_p_list = list(itertools.chain.from_iterable(ballot_perms))
+
+    cand_p_dict ={}
+    for i in cand_p_list:
+        cand_p_dict[i] = 0
+    elections_bt = []
+    diff =[]
+    for i in range(num_elections):
+        bt_ballots = self.bt_ballots()
+        bt_perf_count = Counter(bt_ballots)
+        bt_keys = bt_perf_count.keys()
+        bt_perf_count = cand_p_dict | bt_perf_count
+
+        # Normalize 
+        if normalize == True:
+          for key in bt_perf_count.keys():
+            bt_perf_count[key] = bt_perf_count[key] / self.num_ballots
+
+        elections_bt.append(bt_perf_count)
+
+    self.list_of_ballot_distributions = elections_bt
+    return elections_bt
---
+++ ballot_clustering/electionDistance.py
@@ -0,0 +1,102 @@
+import numpy as np
+from numpy import matrix
+import itertools
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+import ot
+import ot.plot
+import networkx as nx
+from numpy.random import choice
+from collections import Counter
+from ballots import BallotGen
+import time
+from sklearn import manifold
+from sklearn.metrics import euclidean_distances
+from sklearn.decomposition import PCA
+from pref_schedule import pref_schedule
+
+
+# TODO: test 
+
+'''
+This class compares lists of preference schedules, and makes an MDS plot 
+organized by the source of the ballots in the preference scehdules (e.g. PL or BT-generated)
+'''
+
+def earth_mover_dist(electA_distr, electB_distr, cost_matrix):
+        #  Solving Earth Mover Distance
+        G0 = ot.emd(electA_distr,electB_distr, cost_matrix)
+        
+        #  Hadamard Product = Earth mover dist between two matrices 
+        earth_mover_dist = np.sum(np.multiply(cost_matrix, G0))
+        return earth_mover_dist
+
+class electionDistance: 
+
+    # TODO: set generic dist_type that you can invoke 
+
+    # elections_by_type takes list of lists of pref_schedules, organized by type (e.g. PL generated, BT generated) 
+    # election_type_names are used to label MDS plot 
+    def __init__(self, elections_by_type, election_type_names = [], dist_func = earth_mover_dist):
+        self.elections_by_type = elections_by_type
+        self.election_type_names = election_type_names 
+        
+        self.allElections = list(itertools.chain.from_iterable(self.elections_by_type))
+        
+        self.dist_matrix = [[0 for __ in range(len(self.allElections))] for _ in range(len(self.allElections))]
+        for i in range(len(self.allElections)): 
+            electionA_vals = [value for key, value in sorted(self.allElections[i].items())]
+            electA_distr = np.array(electionA_vals)
+            for j in range(i+1,len(self.allElections)):
+                electionB_vals = [value for key, value in sorted(self.allElections[j].items())]
+                electB_distr = np.array(electionB_vals)
+
+                # TODO: cost_matrix gets called from prefschedule class 
+                em_dist = dist_func(electA_distr,electB_distr, pref_schedule.get_cost_matrix())
+                self.dist_matrix[i][j] = em_dist
+                self.dist_matrix[j][i] = em_dist
+
+
+    def generate_MDS_plot(self):
+
+        mds = manifold.MDS(
+            n_components=2,
+            max_iter=3000,
+            eps=1e-9,
+            dissimilarity="precomputed",
+            n_jobs=1,
+            normalized_stress="auto",
+        )
+
+        pos = mds.fit(np.array(self.dist_matrix)).embedding_
+        #print(pos)
+
+        # get indicies for each distribution type set for MDS plotting
+        indicies = []
+        for i, distribution_set in enumerate(self.elections_by_type):
+            if i == 0:
+                indicies.append(len(distribution_set))
+            else:
+                indicies.append(len(distribution_set) + indicies[i - 1])
+
+        point_size = 5
+        colors = ['red', 'green', 'blue', 'orange', 'purple', 'brown', 'pink', 'black', 'turquoise']
+
+        for n, index in enumerate(indicies):
+            if n == 0:
+                plt.scatter(pos[0:index, 0], pos[0:index, 1], color= colors[n], lw=0, s=point_size)
+            else:
+                plt.scatter(pos[indicies[n - 1]:index, 0], pos[indicies[n - 1]:index, 1], color= colors[n], lw=0, s=point_size)
+
+
+        plt.legend(self.election_type_names)
+
+        # TODO: num_elections * 4? 
+       # plt.title(f'Earth Mover Dist: {num_elections} Elections with {len(cand_list)} Candidates')
+        plt.show()
+                        
+    
+
+        
+
+
